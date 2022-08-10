d6ee2667b9c81c1ad6e6466ca7276f90252e5277
Dale Hurd <110054852+dalehurd@users.noreply.github.com>
2022-08-10 12:23:50 -0400

    fine tuned program

--- project code.R
+++ project code.R
@@ -1,28 +1,31 @@
 
-print ("geting started")
 best <- function(state, outcome) {
-  ## Read outcome data
-  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
-  fd   <- as.data.frame(cbind(data[, 2],   # hospital
+  ## Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")  ## define the input data set equal to 'data'
+  subdata   <- as.data.frame(cbind(data[, 2],   # hospital
                               data[, 7],   # state
                               data[, 11],  # heart attack
                               data[, 17],  # heart failure
                               data[, 23]), # pneumonia
                         stringsAsFactors = FALSE)
-  colnames(fd) <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
-  if(!state %in% fd[, "state"]){
+  colnames(subdata) <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+ 
+  if(!is.element (state,subdata[, "state"])) {  ## Check for valid state name
     stop('invalid state')
-  } else if(!outcome %in% c("heart attack", "heart failure", "pneumonia")){
+ 
+  } else if(!is.element(outcome, c("heart attack", "heart failure", "pneumonia"))){  ## Check for valid outcome
     stop('invalid outcome')
   } else {
-    si <- which(fd[, "state"] == state)
-    ts <- fd[si, ]    # extracting data for the called state
-    oi <- as.numeric(ts[, eval(outcome)])
-    min_val <- min(oi, na.rm = TRUE)
-    result  <- ts[, "hospital"][which(oi == min_val)]
-    output  <- result[order(result)]
+    state_index <- which(subdata[, "state"] == state)  ## index values of global dataset for particular state
+    state_data_table <- subdata[state_index, ]    # extracting  all data for the called state
+    outcome_values <- as.numeric(state_data_table[, eval(outcome)]) # extracting only the outcome data for analysis
+    min_val <- min(outcome_values, na.rm = TRUE)  ## finding the minimum value for specified outcome
+    result  <- state_data_table[, "hospital"][which(outcome_values == min_val)] ## finding all locations with minimum values
+  
+    output  <- result[order(result)]  ##Accommodating for ties
   }
+
   return(output)
 }
 
