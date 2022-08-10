6592f3bea071ddf00c401d87cd167c4710b34011
Dale Hurd <110054852+dalehurd@users.noreply.github.com>
2022-08-10 18:55:56 -0400

    much improved

--- rankhospital.R
+++ rankhospital.R
@@ -13,24 +13,22 @@ rankhospital <- function(state, outcome, rank = "best"){
     
   } else if(!is.element(outcome, c("heart attack", "heart failure", "pneumonia"))){  ## Check for valid outcome
     stop('invalid outcome')
-  } else if {
-    state_index <- which(subdata[, "state"] == state)  ## index values of global dataset for particular state
-    state_data_table <- subdata[state_index, ]    # extracting  all data for the called state
+  
   } else if (is.numeric(rank)) {
     
-    state_index <- which(subdata[, "state"] == state)  ## index values of global dataset for particular state
-    state_data_table <- subdata[state_index, ]    # extracting  all data for the called state
-    outcome_values <- as.numeric(state_data_table[, eval(outcome)]) # extracting only the outcome data for analysis
-    min_val <- min(outcome_values, na.rm = TRUE)  ## finding the minimum value for specified outcome
-    result  <- state_data_table[, "hospital"][which(outcome_values == min_val)] ## finding all locations with minimum values
-    
-    output  <- result[order(result)]  ##Alphabetizing
-    
-   ## si <- which(fd[, "state"] == state)
-   ## ts <- fd[si, ]                     # extracting dataframe for the called state
-   ## ts[, eval(outcome)] <- as.numeric(ts[, eval(outcome)])
-   ## ts <- ts[order(ts[, eval(outcome)], ts[, "hospital"]), ]
-   ## output <- ts[, "hospital"][rank]
+    state_index <<- which(subdata[, "state"] == state)  ## index values of global dataset for particular state
+    state_data_table <<- subdata[state_index, ]    ## extracting  all data for the called state
+
+    outcome_values <<- as.numeric(state_data_table[, eval(outcome)]) ## extracting only the outcome data for analysis
+
+    state_data_table[,eval(outcome)] <- as.numeric (state_data_table[,eval(outcome)]) ##Provide an index to the ranking
+    print (state_data_table)
+    state_data_table <- state_data_table[order(state_data_table[, eval(outcome)], state_data_table[, "hospital"]),]
+    output <- state_data_table[, "hospital"][rank]
+   
+  
+
+
   } else if (!is.numeric(rank)){
     if (rank == "best") {
       output <- best(state, outcome)
@@ -44,4 +42,7 @@ rankhospital <- function(state, outcome, rank = "best"){
       stop('invalid rank')
     }
   }
+  
   return(output)
+}
+  #
