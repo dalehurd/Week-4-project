6512b741d21b8936400ef2cfbd402e4f1dad7eb0
Dale Hurd <110054852+dalehurd@users.noreply.github.com>
2022-08-11 08:21:36 -0400

    thursday 821AM

--- rankhospital.R
+++ rankhospital.R
@@ -18,8 +18,8 @@ rankhospital <- function(state, outcome, rank = "best"){
     
     state_index <<- which(subdata[, "state"] == state)  ## index values of global dataset for particular state
     state_data_table <<- subdata[state_index, ]    ## extracting  all data for the called state
-    state_data_table[,eval(outcome)] <- as.numeric (state_data_table[,eval(outcome)]) ##Assure numeric values for outcome
     state_data_table <- state_data_table[order(state_data_table[, eval(outcome)], state_data_table[, "hospital"]),]
+    #state_data_table <- state_data_table[order(state_data_table[, eval(outcome)]),] ties not alhabetized
     output <- state_data_table[, "hospital"][rank]
    
 
